052c6ed9d7cf69ae8af825a8def8bd8ec2f200a7
Shanae Leslie <sleslie18@gmail.com>
2023-01-14 12:33:55 -0500

    save

---
+++ babel.js
@@ -0,0 +1,7 @@
+// ** UNDERSTAND HOW BABEL TRANSFORMS MODERN JS INTO OLDER JS
+
+/*
+    With all the modern JS features we're using, you might wonder... how well are they supported by browsers? Will your users be able to run these apps?
+
+    In this lesson we'll look at what the Babel compiler does behind the scenes to turn your fancy modern JS into a dialect that most every browser can understand.
+*/
